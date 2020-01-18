37f98822d9e601f22b07b2e1764beb16faeb3616
Pavel Padalka <ppadalka.pw@gmail.com>
2020-01-18 14:15:25 +0200

    Code was formatted

--- src/property.ts
+++ src/property.ts
@@ -1,14 +1,14 @@
 import {Comparator} from './comparators';
 
 export interface Property<T> {
-
-    getValue(object: T): any;
-
-    getSortOrder(): SortOrder;
-
-    getComparator(): Comparator<any>
+  
+  getValue(object: T): any;
+  
+  getSortOrder(): SortOrder;
+  
+  getComparator(): Comparator<any>
 }
 
 export enum SortOrder {
-    ASC = 'asc', DESC = 'desc'
+  ASC = 'asc', DESC = 'desc'
 }
